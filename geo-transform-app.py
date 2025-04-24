82dffa3a68c3af95f0552978b5ce5f66e41b40cf
mohan11333 <24amtics038@gmail.com>
2025-04-24 18:22:49 +0530

    geo-transform-app.py

--- geo-transform-app.py
+++ geo-transform-app.py
@@ -63,20 +63,20 @@ if st.button("Load & Transform Data"):
     ax1.set_title("Original Price Curve")
     st.pyplot(fig1)
 
-    # Get transformation matrix input
-    st.subheader("Enter 2D Transformation Matrix")
-    a11 = st.number_input("a11", value=1.0)
-    a12 = st.number_input("a12", value=0.0)
-    a21 = st.number_input("a21", value=0.0)
-    a22 = st.number_input("a22", value=1.0)
+   transformation_type = st.selectbox("Select Transformation Type", ["None", "Scale", "Rotate", "Translate"])
 
-    transform = np.array([[a11, a12], [a21, a22]])
-
-    # Apply transformation
-    transformed_points = transform @ points
-
-    st.subheader("Transformed Plot")
-    fig2, ax2 = plt.subplots()
-    ax2.plot(transformed_points[0], transformed_points[1])
-    ax2.set_title("Transformed Curve")
-    st.pyplot(fig2)
+if transformation_type == "Scale":
+    scale_factor = st.number_input("Scale Factor", value=1.0)
+    transform = np.array([[scale_factor, 0], [0, scale_factor]])
+elif transformation_type == "Rotate":
+    angle = st.number_input("Rotation Angle (degrees)", value=0.0)
+    radians = np.radians(angle)
+    transform = np.array([[np.cos(radians), -np.sin(radians)], [np.sin(radians), np.cos(radians)]])
+elif transformation_type == "Translate":
+    tx = st.number_input("Translate X", value=0.0)
+    ty = st.number_input("Translate Y", value=0.0)
+    transform = np.array([[1, 0], [0, 1]])  # Translation is not a linear transformation, handle separately
+    transformed_points[0] += tx
+    transformed_points[1] += ty
+else:
+    transform = np.array([[1, 0], [0, 1]])  # Identity transformation
